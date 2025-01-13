adb656c434b489ad85dd9e32ef901fe31c54fb9a
saumyasingh003 <saumyasingh98982@gmail.com>
2025-01-13 23:54:15 +0530

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,14 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import logo from "../public/icons/logo.svg"
 
 const Navbar = () => {
   return (
     <nav className=" flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
-          src="/icons/logo.svg"
+          src={logo}
           width={32}
           height={32}
           alt="zoomiii logo"
