9c560e5c0c9537a4fc8be3f04bf480f8c77614af
saumyasingh003 <saumyasingh98982@gmail.com>
2025-01-13 23:20:45 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: ['https://zoomii.vercel.app',"htttp://locahost:3000"], 
-  // },
+  images: {
+    domains: ['https://zoomii.vercel.app',"htttp://locahost:3000"], 
+  },
+  output: "standalone",
+  reactStrictMode: false,
 };
 
 export default nextConfig;
